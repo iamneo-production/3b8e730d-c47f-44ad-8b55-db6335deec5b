26412236440735f13422c5aa97fcf76eab9b38e4
ashwaniKsingla <ashwanikum@zeta.tech>
2022-07-08 03:19:05 +0530

    Added spring boot securiy

--- springapp/src/main/java/com/examly/springapp/HelloWorld.java
+++ springapp/src/main/java/com/examly/springapp/HelloWorld.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class HelloWorld {
     
-    @GetMapping("/home")
+    @GetMapping("/old-home")
     public String home(){
         return "hello world 3";
     }
---
+++ springapp/src/main/java/com/examly/springapp/ViewController.java
@@ -0,0 +1,30 @@
+package com.examly.springapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+  @GetMapping("/home")
+  public String getHome() {
+    return "home";
+  }
+
+  @GetMapping("/")
+  public String getHomeData() {
+    return "home";
+  }
+
+  @GetMapping("/hello")
+  public String getHello() {
+    return "hello";
+  }
+
+  @GetMapping("/login")
+  public String getLogin() {
+    return "login";
+  }
+
+
+}
